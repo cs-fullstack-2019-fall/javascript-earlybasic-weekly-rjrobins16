8e922f2b71515bb84cafa5e9be7808b29b19b209
rjrobins16 <rjrobins16@gmail.com>
2019-08-09 16:52:28 -0500

    review

---
+++ project.js
@@ -0,0 +1,46 @@
+
+var entry=prompt("Welcome to Rachel's Website. Enter 1 to count. Enter 2 to use the calculator.");
+
+
+if (entry==1)
+
+{
+	var numberEntry=prompt("Count by ones. Enter a number.");
+
+	var newCount=numberEntry
+
+	while (newCount > numberEntry)
+	{
+		if (newCount!==numberEntry)
+		console.log(numberEntry);
+		}
+
+
+	console.log(numberEntry)
+	console.log
+	;
+
+}
+	// kept bugging after trying for hours. gave up.
+
+
+
+
+
+
+else if (entry==2)
+
+{
+	var num1= prompt("Enter a number.");
+
+	var num2= prompt("Enter another number.)"
+
+var numberInt= parseInt(num1)
+var numberInt2= parseInt(num2)
+ 
+sum = numberInt + numberInt2
+
+//Number entry.
+
+
+
